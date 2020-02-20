11e7529ec1bdda9a792ae52d31c9a043766e866a
twomeetings <2326220@163.com>
2020-02-20 17:03:36 +0800

    chore: update actions helper

--- src/core/actionsHelper.ts
+++ src/core/actionsHelper.ts
@@ -1,17 +1,22 @@
 import { createAction } from 'redux-actions'
-// import _ from 'lodash'
 
-// const actionMap = {}
+const actionMap = {}
 
-export const createActions = <T extends { [key: string]: Function }>(
-  obj,
-  prefix = ''
+export const createActions = <T extends { [key: string]: Function, }>(
+  obj: T,
+  prefix: string = ''
 ) => {
-  // const objProxy = {}
   const objProxy: {
     [K in Extract<keyof T, string>]: any
   } = {} as any
   Object.keys(obj).forEach(e => {
+    if (actionMap.hasOwnProperty(e)) {
+      throw new Error(`action - '${e}' is duplicated`)
+    } else {
+      // 0表示action存在，但尚未和任何reducer关联。
+      actionMap[e] = 0
+    }
+
     const actionType = `${prefix}_${e}`
 
     // @ts-ignore
@@ -19,5 +24,38 @@ export const createActions = <T extends { [key: string]: Function }>(
     objProxy[e].toString = () => actionType
   })
 
+  // const objProxyWrapper = originalObj => {
+  //   return new Proxy(originalObj, {
+  //     get(target, property) {
+  //       const val = target[property]
+  //       if (val === undefined) {
+  //         throw new Error(`the action '${property}' is not defined`)
+  //       }
+  //
+  //       // 1表示action存在，并且已有对应reducer处理
+  //       actionMap[property] = 1
+  //       return val
+  //     },
+  //   })
+  // }
+
+  // 某些环境下Proxy不存在，比如IE浏览器和React Native运行环境。
+  // if (typeof Proxy === 'function') {
+  //   return objProxyWrapper(objProxy)
+  // }
+
   return objProxy
 }
+
+// export const showActionsRefState = () => {
+//   const unusedActions = []
+//   Object.keys(actionMap).forEach(name => {
+//     if (actionMap[name] !== 1) {
+//       unusedActions.push(name)
+//     }
+//   })
+
+//   if (unusedActions.length > 0) {
+//     console.warn('currently, the unused actions are:', unusedActions)
+//   }
+// }
